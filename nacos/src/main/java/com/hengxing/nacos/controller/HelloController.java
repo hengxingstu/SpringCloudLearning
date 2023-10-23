dd4ac22f5521bc0a193251f333fa458d4da97f69
hengxing <hengxingstu@163.com>
2023-10-23 09:07:23 +0800

    nacosServer配置

--- nacos/src/main/java/com/hengxing/nacos/controller/HelloController.java
+++ nacos/src/main/java/com/hengxing/nacos/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.hengxing.nacos.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 10/21/2023 20:00:24
  */
 @RestController
-@RefreshScope
+//@RefreshScope
 public class HelloController {
     @Value("${spring.datasource.username}")
     String name;
